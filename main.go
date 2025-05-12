aa27e6a3bf1885f915532df7cbac25c58cc0930a
= <sharukhprogramming@gmail.com>
2025-05-12 22:19:41 +0530

    added post and user structure

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main 
+
+type User struct {
+	FullName string `json:"fullName"`
+	UserName string `json:"userName"`
+	Email string `json:"email"`
+}
+
+type Post struct {
+	Title string `json:"title"`
+	Body string `json:"body"`
+	Author string `json:"author"`
+}
+
+func main(){
+	
+}
